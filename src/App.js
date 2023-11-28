bc7a2388857f46ba0a5add11a10e2e3d96586fe8
SergeyB <brynkin.dev@gmail.com>
2023-11-28 12:40:40 +0300

    add promotion page template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.scss";
 import {Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import PromotionPage from "./pages/PromotionPage/PromotionPage";
 
 function App() {
     return (
         <Routes>
             <Route path={'/'} element={<Layout/>}>
-                <Route index element={<div></div>}/>
+                <Route index element={<PromotionPage/>}/>
                 <Route path={':id'} element={<div></div>}/>
             </Route>
         </Routes>
