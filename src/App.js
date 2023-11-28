50f89827f859e9b14ca9a6e668b3b3f0642f7027
SergeyB <brynkin.dev@gmail.com>
2023-11-28 10:21:33 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -8,16 +8,6 @@ function App() {
         <Route path={'/'} element={<Layout/>}>
             <Route index element={<div>index</div>}/>
             <Route path={':id'} element={<div></div>}/>
-            {/*<Route path={'products'} element={<div>Products</div>}/>*/}
-            {/*<Route path={'users'} element={<div>users</div>}/>*/}
-            {/*<Route path={'categories'} element={<div>categories</div>}/>*/}
-            {/*<Route path={'cities'} element={<div>cities</div>}/>*/}
-            {/*<Route path={'brands'} element={<div>brands</div>}/>*/}
-            {/*<Route path={'protocols'} element={<div>protocols</div>}/>*/}
-            {/*<Route path={'orders'} element={<div>orders</div>}/>*/}
-            {/*<Route path={'banners'} element={<div>banners</div>}/>*/}
-            {/*<Route path={'seminars'} element={<div>seminars</div>}/>*/}
-            {/*<Route path={'promocodes'} element={<div>promocodes</div>}/>*/}
         </Route>
     </Routes>
   );
