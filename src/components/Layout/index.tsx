7c0e0b6476b1e0923b9704d2c8069644bde31415
SergeyB <brynkin.dev@gmail.com>
2023-12-21 16:20:06 +0300

    move routes to separate constant

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,6 +5,7 @@ import navLinks from '../../constants/navlinks'
 import PromotionPage from '../../pages/PromotionPage'
 import ClientsPage from '../../pages/ClientsPage'
 import CategoriesPage from '../../pages/CategoriesPage'
+import { routes } from '../../constants/routes'
 
 const Layout = () =>
   (<div className={styles.layout}>
@@ -14,16 +15,16 @@ const Layout = () =>
     <main className={styles.content}>
       <Routes>
         <Route index element={<></>}/>
-        <Route path={'products'} element={<div>Products</div>}/>
-        <Route path={'users'} element={<ClientsPage/>}/>
-        <Route path={'categories'} element={<CategoriesPage/>}/>
-        <Route path={'cities'} element={<div>cities</div>}/>
-        <Route path={'brands'} element={<div>brands</div>}/>
-        <Route path={'protocols'} element={<div>protocols</div>}/>
-        <Route path={'orders'} element={<div>orders</div>}/>
-        <Route path={'banners'} element={<div>banners</div>}/>
-        <Route path={'seminars'} element={<div>sections</div>}/>
-        <Route path={'promotions'} element={<PromotionPage/>}/>
+        <Route path={routes.products} element={<div>Products</div>}/>
+        <Route path={routes.users} element={<ClientsPage/>}/>
+        <Route path={routes.categories} element={<CategoriesPage/>}/>
+        <Route path={routes.cities} element={<div>cities</div>}/>
+        <Route path={routes.brands} element={<div>brands</div>}/>
+        <Route path={routes.protocols} element={<div>protocols</div>}/>
+        <Route path={routes.orders} element={<div>orders</div>}/>
+        <Route path={routes.banners} element={<div>banners</div>}/>
+        <Route path={routes.seminars} element={<div>sections</div>}/>
+        <Route path={routes.promotions} element={<PromotionPage/>}/>
       </Routes>
     </main>
   </div>)
---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+const routes = {
+  products: 'products',
+  users: 'users',
+  categories: 'categories',
+  cities: 'cities',
+  brands: 'brands',
+  protocols: 'protocols',
+  orders: 'orders',
+  banners: 'banners',
+  seminars: 'seminars',
+  promotions: 'promotions'
+}
+
+export {
+  routes
+}
